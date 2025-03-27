f4c7013eb8b5871c5f193762d0c05d4cae35ddea
tamirbviral <tamirbviral@gmail.com>
2025-03-27 12:17:10 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,22 +1,21 @@
 export default {
   async fetch(request) {
-    const url = new URL(request.url);
+    const url = new URL(request.url)
 
-    if (url.pathname.match(/\.(woff2?|ttf|otf|eot)$/)) {
-      const response = await fetch(request);
-      const newHeaders = new Headers(response.headers);
+    // שים לב – מחליף את fonts.bviral.co.il ל-www.bviral.co.il
+    url.hostname = "www.bviral.co.il"
 
-      newHeaders.set("Access-Control-Allow-Origin", "https://dev.bviral.co.il");
-      newHeaders.set("Access-Control-Allow-Methods", "GET, OPTIONS");
-      newHeaders.set("Access-Control-Allow-Headers", "*");
+    // שולח את הבקשה ל-www
+    const response = await fetch(url.toString(), request)
 
-      return new Response(response.body, {
-        status: response.status,
-        statusText: response.statusText,
-        headers: newHeaders,
-      });
-    }
+    // מחזיר את התגובה, עם CORS פתוח
+    const newHeaders = new Headers(response.headers)
+    newHeaders.set("Access-Control-Allow-Origin", "*")
 
-    return fetch(request);
-  },
-};
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: newHeaders,
+    })
+  }
+}
